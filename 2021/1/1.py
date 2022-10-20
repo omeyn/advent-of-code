05d03380bda18b9174a3b05a70bb8b2df75ad074
Oliver Meyn <oliver@mineallmeyn.com>
2022-10-19 23:12:39 -0400

    day 1

---
+++ 2021/1/1.py
@@ -0,0 +1,39 @@
+import os  # first you need to import the module 'os'
+
+# set the cwd to 'chapter10'
+os.chdir('2021/1')
+
+def star1():
+    f = open("1.input", "r")
+    prev = None
+    count = 0
+    for line in f:
+        num = int(line)
+        if (prev):
+            if (num - prev) > 0:
+                count = count + 1
+        prev = num
+
+    print("Day 1 Star 1: " + str(count))
+
+def star2():
+    f = open("1.input", "r")
+    count = 0
+    nums = []
+    for line in f:
+        # print(line)
+        nums.append(int(line))
+
+    for i in range(len(nums)):
+        if (i - 3 >= 0):
+            sum_a = nums[i-1] + nums[i-2] + nums[i-3]
+            sum_b = nums[i] + nums[i-1] + nums[i-2]
+            # print(str(sum_a) + ' vs ' + str(sum_b))
+            if (sum_b - sum_a > 0):
+                # print('hit')
+                count = count + 1
+
+    print("Day 1 Star 2: " + str(count))
+
+star1()
+star2()
