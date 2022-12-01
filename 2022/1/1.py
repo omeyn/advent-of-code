caf4fb1f0d3843e6d140cc526fcf6a0103d9e003
omeyn-recursion <oliver.meyn@recursionpharma.com>
2022-12-01 09:41:52 -0500

    day 1 done

--- 2022/1/1.py
+++ 2022/1/1.py
@@ -1,11 +1,24 @@
 import os
 
-os.chdir('2022/1')
+os.chdir('1')
 
 def star1():
+    sums = []
     f = open("1.input", "r")
+    sum = 0
     for line in f:
-        print(f)
+        if line.strip() == '':
+            sums.append(sum)
+            sum = 0
+        else:
+            sum += int(line.strip())
+
+    print(f"Single max: {max(sums)}")
+
+    sums.sort()
+    print(f"Sorted sums {sums}")
+    top3 = sums[-1] + sums[-2] + sums[-3]
+    print(f"Top 3 summed {top3}")
+
 
 star1()
-# star2()
