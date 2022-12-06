d09b2ca30c8e44c15d5098276d66825f855e7fda
Oliver Meyn <oliver@mineallmeyn.com>
2022-12-06 17:51:56 -0500

    finish 2020 day 3

--- 2020/3.py
+++ 2020/3.py
@@ -2,16 +2,7 @@ import os
 
 os.chdir('2020')
 
-def star1(filename):
-    trees = []
-    tree_template = []
-    f = open(filename, "r")
-    for line in f:
-        clean = line.strip()
-        row = [1 if x == '#' else 0 for x in clean]
-        trees.append(row)
-        tree_template.append(row)
-
+def find_slope(trees, delta_x, delta_y):
     print(trees)
     # goal is reach y = len(trees)-1
     x = 0
@@ -24,9 +15,29 @@ def star1(filename):
         print(f"checking y {y} x {x}")
         print(f"current tree shape is y {len(trees)} x {len(trees[0])}")
         treecount += trees[y][x]
-        x += 3
-        y += 1
+        x += delta_x
+        y += delta_y
     print(f"Got tree count {treecount}")
+    return treecount
+
+def star1(filename):
+    trees = []
+    tree_template = []
+    f = open(filename, "r")
+    for line in f:
+        clean = line.strip()
+        row = [1 if x == '#' else 0 for x in clean]
+        trees.append(row)
+        tree_template.append(row)
+
+    tree_product = 1
+    tree_product *= find_slope(trees, 1, 1)
+    tree_product *= find_slope(trees, 3, 1)
+    tree_product *= find_slope(trees, 5, 1)
+    tree_product *= find_slope(trees, 7, 1)
+    tree_product *= find_slope(trees, 1, 2)
+    print(f"tree product {tree_product}")
+
 
 filename = "3.input"
 # filename = "3-test.input"
