e0ebee7fd6c49900ae4bb1c7b7392d6a1b652624
Oliver Meyn <oliver@mineallmeyn.com>
2022-12-11 13:48:03 -0500

    terrible day 9 2022 hack

--- 2022/9/9.py
+++ 2022/9/9.py
@@ -20,13 +20,27 @@ def touching(h, t):
 
 def viz(h, t):
     max_x = max(int(h[0]), int(t[0]))
-    min_x = min(int(h[0]), int(t[0]))
+    # min_x = min(int(h[0]), int(t[0]))
     max_y = max(int(h[1]), int(t[1]))
-    min_y = min(int(h[1]), int(t[1]))
-    grid = [['.' for x in range(max_x +2 - min_x)] for y in range(max_y + 2 - min_y)]
-    for y in range(len(grid)):
-        for x in range(len(grid[0])):
-            print(grid[y][x])
+    # min_y = min(int(h[1]), int(t[1]))
+    # print(h)
+    # print(t)
+    # grid = [['.' for x in range(max_x +5 - min_x)] for y in range(max_y + 5 - min_y)]
+    grid = [['.' for x in range(max_x +5)] for y in range(max_y + 5)]
+    # h_x = int(h[0])-min_x
+    # h_y = int(h[1])-min_y
+    # t_x = int(t[0])-min_x
+    # t_y = int(t[1])-min_y
+    h_x = int(h[0])
+    h_y = int(h[1])
+    t_x = int(t[0])
+    t_y = int(t[1])
+    print(h_x)
+    print(h_y)
+    grid[h_x][h_y] = 'H' 
+    grid[t_x][t_y] = 'T' 
+    for y in range(len(grid)):  
+        print(''.join(grid[y]))
 
 
 
@@ -40,7 +54,7 @@ def move(t, h, fields, t_visits):
         print(f"fields {fields} with diffx {diff_x} and diffy {diff_y}")
         assert abs(diff_x) == 1 or abs(diff_y) == 1
         # need diagonal
-        if diff_x == 1:
+        if abs(diff_x) == 1:
             if diff_y > 0:
                 t = (h[0], t[1]+1)
             else:
@@ -58,7 +72,7 @@ def move(t, h, fields, t_visits):
 
     diff_x = h[0] - t[0]
     diff_y = h[1] - t[1]
-    # print(f"new diffx {diff_x} diffy {diff_y}")
+    print(f"new diffx {diff_x} diffy {diff_y}")
     if diff_x == 0:
         # move in y
         if diff_y > 0:
@@ -66,7 +80,7 @@ def move(t, h, fields, t_visits):
                 t = (t[0], t[1]+1)
                 t_visits.add(t)
         else:
-            for x in range(abs(diff_x)-1):
+            for x in range(abs(diff_y)-1):
                 t = (t[0], t[1]-1)
                 t_visits.add(t)
         print(f"vert move for t to {t}")
@@ -82,9 +96,6 @@ def move(t, h, fields, t_visits):
                 t_visits.add(t)
         print(f"horiz move for t to {t}")
     return t
-
-
-    
 
 def star1(filename):
     f = open(filename, 'r')
@@ -109,18 +120,27 @@ def star1(filename):
             case 'D':
                 h = (h[0], h[1] - int(fields[1]))
         print(f"moved H to {h}")
+        # viz(h,t)
         t = move(t, h, fields, t_visits)
         print(f"moved T to {t}")
+        # viz(h,t)
+        assert touching(h,t)
+        print(f"T move count {len(t_visits)}")
 
 
     print(f"star 1 result {len(t_visits)}")
 
+# 5312 too low
+# 6091 too high
+# 6090 is right - pure guess. Off by one somewhere!
+
 filename = "9.input"
+# filename = "9-test2.input"
 # filename = "9-test.input"
-# star1(filename)
+star1(filename)
 # star2(filename)
 
 # test_touching()
-h = (10,10)
-t = (11,11)
-viz(h,t)
+# h = (10,10)
+# t = (11,11)
+# viz(h,t)
