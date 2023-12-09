8f26fde06e18d38995c29b91fcd71544354872cf
Oliver Meyn <oliver@elephant.tech>
2023-12-09 13:48:06 -0500

    finish 5

--- 2023/5/5b.py
+++ 2023/5/5b.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# os.chdir('2023/5')
+os.chdir('2023/5')
 
 # tried the dumb way and didn't work, took 12 hrs?
 # got new min location 1852510996
@@ -12,6 +12,8 @@ import sys
 # got new min location 24261546
 # min location is 24261546
 
+# cheating, result should be 24261545 (OFF BY FUCKING ONE!)
+
 class Range:
     def __init__(self, source_range_start: int, dest_range_start: int, range_size: int):
         self.source_range_start = source_range_start
@@ -19,7 +21,7 @@ class Range:
         self.range_size = range_size
 
     def is_in_range(self, source):
-        return source >= self.source_range_start and source <= self.source_range_start + self.range_size
+        return source >= self.source_range_start and source < self.source_range_start + self.range_size
     
     def find_dest(self, source):
         return self.dest_range_start + (source - self.source_range_start)
@@ -40,6 +42,12 @@ class RangedLookup:
         # not in any ranges, return source as the dest
         return source
 
+def is_valid_seed(seed, seed_sets):
+    for seed_set in seed_sets:
+        if seed in range(seed_set[0], seed_set[0]+seed_set[1]):
+            return True
+        
+    return False
 
 def star1(input_file):
     f = open(input_file, "r")
@@ -54,7 +62,8 @@ def star1(input_file):
             max_seed = raw_seeds[i] + raw_seeds[i+1]
         seed_sets.append([raw_seeds[i], raw_seeds[i+1]])
 
-    print(f'min seed {min_seed} max seed {max_seed}')
+    # print(f'min seed {min_seed} max seed {max_seed}')
+    print(seed_sets)
 
     f.readline()
     lookups = {}
@@ -67,29 +76,58 @@ def star1(input_file):
                 lookups[map_name] = ranged_lookup
             else:
                 nums = [int(x) for x in line.strip().split(' ')]
-                new_range = Range(source_range_start=nums[1], dest_range_start=nums[0], range_size=nums[2])
+                new_range = Range(source_range_start=nums[0], dest_range_start=nums[1], range_size=nums[2])
                 ranged_lookup.add_range(new_range)
 
     # print(lookups)
     min_location = sys.maxsize
-    for seed_set in seed_sets:
-        for i in range(seed_set[1]+1):
-            seed = seed_set[0] + i
-            soil = lookups['seed-to-soil'].find_dest_for_source(seed)
-            fert = lookups['soil-to-fertilizer'].find_dest_for_source(soil)
-            water = lookups['fertilizer-to-water'].find_dest_for_source(fert)
-            light = lookups['water-to-light'].find_dest_for_source(water)
-            temp = lookups['light-to-temperature'].find_dest_for_source(light)
-            humid = lookups['temperature-to-humidity'].find_dest_for_source(temp)
-            location = lookups['humidity-to-location'].find_dest_for_source(humid)
-        #     print(f"seed {seed} soil {soil} fert {fert} water {water} light {light} temp {temp} humid {humid} location {location}")
-            if location < min_location:
-                print(f'got new min location {location} for seed {seed}')
-                min_location = location
+    location = 24000000
+    while True:
+        location += 1
+        if location % 500000 == 0:
+            print(f'checked {location} locations')
+        humid = lookups['humidity-to-location'].find_dest_for_source(location)
+        temp = lookups['temperature-to-humidity'].find_dest_for_source(humid)
+        light = lookups['light-to-temperature'].find_dest_for_source(temp)
+        water = lookups['water-to-light'].find_dest_for_source(light)
+        fert = lookups['fertilizer-to-water'].find_dest_for_source(water)
+        soil = lookups['soil-to-fertilizer'].find_dest_for_source(fert)
+        seed = lookups['seed-to-soil'].find_dest_for_source(soil)
+        
+        # print(f"seed {seed} soil {soil} fert {fert} water {water} light {light} temp {temp} humid {humid} location {location}")
 
-    print(f'min location is {min_location}')
+        if is_valid_seed(seed, seed_sets):
+            print(f'got valid seed at location {location} - thats our min!')
+            print(f"seed {seed} soil {soil} fert {fert} water {water} light {light} temp {temp} humid {humid} location {location}")
+            break
 
 
-input = "5-test.input"
-input = "5-10.input"
+def test():
+    seed_sets = [[0, 10], [15,5], [79,14]]
+    seed = 0
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 5
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 10
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 11
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 15
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 19
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 20
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 21
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 92
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+    seed = 93
+    print(f'seed {seed} is {is_valid_seed(seed, seed_sets)}')
+
+# test()
+
+# input = "5-test.input"
+# input = "5-test2.input"
+input = "5.input"
 star1(input)
